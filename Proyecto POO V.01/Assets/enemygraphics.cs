d1525c5e5176e4fa4c97563dd8b163c24b87fd73
Boggi174 <57078913+Boggi174@users.noreply.github.com>
2020-11-02 22:27:00 -0600

    Update V2
    Se agrega :
    -Enemies with collider boxes
    -PathFinding AI a enemigos
    -Camera Movement
    -Ammo Explosion

---
+++ Proyecto POO V.01/Assets/enemygraphics.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class enemygraphics : MonoBehaviour
+{
+    public AIPath aiPath ; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(aiPath.desiredVelocity.x >= 0.1f)
+        {
+            transform.localScale = new Vector3(-4f,4f,4f);
+        }else if (aiPath.desiredVelocity.x <= -0.1f){
+            transform.localScale = new Vector3(4f,4f,4f);
+        }
+        
+    }
+}
