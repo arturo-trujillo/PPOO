0029f90b48b38faf457658254bef7d08819b138b
Boggi174 <57078913+Boggi174@users.noreply.github.com>
2020-11-16 20:48:59 -0600

    Version 3
    Las balas ya hacen daño a los enemigos y el jugador muere tras ser impactado por los enemigos

--- Proyecto POO V.01/Assets/Scripts/Bullet.cs
+++ Proyecto POO V.01/Assets/Scripts/Bullet.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject hitEffect;
+    public int damage= 30 ;
+    public Rigidbody2D rb;
 
    void OnCollisionEnter2D(Collision2D collision)
    {
+      if(collision.gameObject.tag == "Enemy"){
+          
+           collision.gameObject.SendMessage("TakeDamage", damage);
+      } 
        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
        Destroy(effect, 5f);
        Destroy(gameObject);
    }
+
 }
