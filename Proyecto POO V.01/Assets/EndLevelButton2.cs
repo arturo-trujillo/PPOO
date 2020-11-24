8f9146b12d7fe45c1a3503a3ef412ba0bde6bafb
Boggi174 <57078913+Boggi174@users.noreply.github.com>
2020-11-23 23:44:03 -0600

    POO V7
    Link entre los niveles

---
+++ Proyecto POO V.01/Assets/EndLevelButton2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelButton2 : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision){
+     if(collision.gameObject.tag == "Player")
+      {  
+        Application.LoadLevel("Nivel 3");
+
+      } 
+    }
+}
